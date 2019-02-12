e4f74be8eb8652f19b6fb7c720a76f213df9cf92
railsapp <amankumar1196@gmail.com>
2019-02-12 17:19:53 +0530

    action: index is created along with its template

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,9 +1,16 @@
 class ArticlesController < ApplicationController
+    
+    def index
+        @articles= Article.all
+    end
+
     def new
         @article = Article.new
     end
 
-    
+    def edit
+        @article = Article.find(params[:id])
+    end
     def create
         #render plain: params[:article].inspect
         @article = Article.new(article_params)
@@ -14,6 +21,16 @@ class ArticlesController < ApplicationController
             render 'new' 
         end
 
+    end
+
+    def update
+        @article =Article.find(params[:id])
+        if @article.update(article_params)
+            flash[:notice] ="Article was successfully edited"
+            redirect_to article_path(@article)
+        else
+            render 'edit'
+        end
     end
 
     def show
