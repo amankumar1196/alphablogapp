4035b5e56476602ddbf810be9d7b05aa35ed9a66
railsapp <amankumar1196@gmail.com>
2019-02-19 16:59:35 +0530

    Add edit category to show template

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -18,6 +18,20 @@ class CategoriesController < ApplicationController
             render 'new'
         end
     end
+
+    def edit
+        @category = Category.find(params[:id])
+    end
+
+    def update
+        @category = Category.find(params[:id])
+        if @category.update(category_params) 
+            flash[:success] = "Category name was successfully updated"
+            redirect_to cataegory_path(@category)
+        else
+            render 'edit'
+        end
+    end
     
     def show
         @category = Category.find(params[:id])
