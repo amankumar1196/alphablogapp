35f1bd4f24a0447070050382f4df4c9160ced0de
railsapp <amankumar1196@gmail.com>
2019-02-19 17:23:53 +0530

    Category Update error resolved

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -27,7 +27,7 @@ class CategoriesController < ApplicationController
         @category = Category.find(params[:id])
         if @category.update(category_params) 
             flash[:success] = "Category name was successfully updated"
-            redirect_to cataegory_path(@category)
+            redirect_to category_path(@category)
         else
             render 'edit'
         end
