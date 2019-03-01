2a5a8cf2bfa1936f9050d11dcc8beba433cdf8f9
railsapp <amankumar1196@gmail.com>
2019-03-01 12:56:51 +0530

    Email config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,10 +64,20 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "AlphaBlog_#{Rails.env}"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.default_options = {from: 'amankumar1196@gmail.com'}
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    user_name: 'amankumar1196@gmail.com',
+    password: 'Pul$@r150',
+    authentication: 'plain',
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
-  # config.action_mailer.raise_delivery_errors = false
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
